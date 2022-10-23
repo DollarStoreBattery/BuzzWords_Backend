3106f84ed351434f212f1540c4d0da93b8cdf637
Wajiha Shah <41492355+DollarStoreBattery@users.noreply.github.com>
2022-10-23 17:19:43 -0400

    removed naughty words

--- src/fileCreators/constructWordList.ts
+++ src/fileCreators/constructWordList.ts
@@ -2,6 +2,7 @@ import { readFileSync, writeFileSync } from "fs";
 import { MINIMUM_WORD_LENGTH } from "../constants";
 import { resolve } from "path";
 import { WORD_LIST_FOLDER } from "./common";
+import profaneWords from "../wordlists/profaneWords.json";
 
 const inputFilePath = resolve(WORD_LIST_FOLDER, "2of12inf.txt");
 
@@ -10,7 +11,10 @@ const outputFilePath = resolve(WORD_LIST_FOLDER, "wordList.json");
 const filteredWordList: Array<string> = readFileSync(inputFilePath, "utf8")
   .split("\r\n")
   .filter(
-    (word: string) => !word.includes("s") && word.length >= MINIMUM_WORD_LENGTH
+    (word: string) =>
+      word.length >= MINIMUM_WORD_LENGTH &&
+      !word.includes("s") &&
+      !profaneWords.includes(word)
   );
 
 writeFileSync(outputFilePath, JSON.stringify(filteredWordList));
